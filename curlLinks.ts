79c01cb34588a92d72b5cd30c729afdf7fc37f12
Edygar de Lima Oliveira <edygardelima@gmail.com>
2020-05-27 14:37:43 +0100

    Move function to its own file

---
+++ curlLinks.ts
@@ -0,0 +1,51 @@
+/// @deno-types="./typings/cheerio.d.ts"
+import cheerio from "https://dev.jspm.io/npm:cheerio@0.22.0/index.js";
+
+/**
+ * Curl the given target URLs and return all links
+ */
+export async function curlLinks(targets: Array<string>, { logErrors = true } = {}): Promise<Array<string>> {
+  const lookups = new Set(targets)
+  const found: Set<string> = new Set()
+
+  for await (let url of lookups) {
+    try {
+      url = new URL(url).toString();
+    } catch (e) {
+      $: for (let scheme of ['https://', 'http://']) {
+        if (!url.startsWith(scheme)) {
+          const newUrl = `${scheme}${url}`
+          if (!lookups.has(newUrl)) {
+            lookups.add(newUrl)
+          } else {
+            continue $;
+          }
+        }
+      }
+      continue;
+    }
+
+    try {
+      const request = await fetch(url);
+      const html = await request.text();
+      const $: CheerioStatic = cheerio.load(html);
+
+      Array.from($("a"), (a: CheerioElement) => a.attribs.href).forEach((href) => {
+        try {
+          if (href)
+            found.add(new URL(href, url).toString())
+        } catch(e) {
+          if (logErrors)
+            console.error('Invalid url:', href)
+        }
+      })
+    } catch (e) {
+      if (logErrors) {
+        console.error("Error while trying to fetch", url)
+        console.error(e)
+      }
+    }
+  }
+
+  return Array.from(found.values());
+}
--- mod.ts
+++ mod.ts
@@ -1,45 +1,4 @@
-/// @deno-types="./typings/cheerio.d.ts"
-import cheerio from "https://dev.jspm.io/npm:cheerio@0.22.0/index.js";
+import { curlLinks } from "./curlLinks.ts";
 
-const target = new Set(Deno.args.slice(0));
-const found = new Set()
-
-for await (let url of target) {
-  try {
-    url = new URL(url).toString();
-  } catch (e) {
-    $: for (let scheme of ['https://', 'http://']) {
-      if (!url.startsWith(scheme)) {
-        const newUrl = `${scheme}${url}`
-        if (!target.has(newUrl)) {
-          target.add(newUrl)
-        } else {
-          continue $;
-        }
-      }
-    }
-    continue;
-  }
-
-  try {
-  const request = await fetch(url);
-  const html = await request.text();
-  const $: CheerioStatic = cheerio.load(html);
-
-  Array.from($("a"), (a: CheerioElement) => a.attribs.href).forEach((href) => {
-    try {
-      if (href)
-        found.add(new URL(href, url).toString())
-    } catch(e) {
-      console.error('Invalid url:', href)
-    }
-  })
-  } catch (e) {
-    console.error("Error while trying to fetch", url)
-    console.error(e)
-  }
-}
-
-console.log(
-  Array.from(found.values()).join("\n"),
-);
+const links = await curlLinks(Deno.args.slice(0))
+console.log(links.join("\n"));
